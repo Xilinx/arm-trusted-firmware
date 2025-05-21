0b41e78f2819b18fae18a00fffe243f870d47480
Suraj Kakade <suraj.hanumantkakade@amd.com>
2025-05-21 12:39:13 +0530

    fix(xilinx/common): fixing HIS_COMF violations
    This corrects HIS_COMF violation:
    Relationship of the number of comments (outside of and within functions)
    to the number of statements.
    
    Change-Id: Ia69450ba47ce451fbf85ef75cf38a3a1d975402b
    Signed-off-by: Suraj Kakade <suraj.hanumantkakade@amd.com>
    State: not-upstreamable (fusa)
    Link: https://review.trustedfirmware.org/c/TF-A/trusted-firmware-a/+/49760

--- plat/xilinx/common/plat_clkfunc.c
+++ plat/xilinx/common/plat_clkfunc.c
@@ -11,16 +11,37 @@
 #include <plat_clkfunc.h>
 #include <plat_private.h>
 
+/*
+ * plat_get_syscnt_freq2 - Retrieves the system counter frequency.
+ *
+ * This function reads the system counter frequency from a memory-mapped register
+ * located at IOU_SCNTRS_BASE + IOU_SCNTRS_BASE_FREQ_OFFSET. If the read value is
+ * non-zero, it is considered valid and returned. Otherwise, it logs a message
+ * indicating that the counter frequency was not properly initialized (i.e., value is 0),
+ * and defaults to using the `cpu_clock` as a fallback value for system counter frequency.
+ * This ensures the platform always has a usable frequency value, preventing runtime
+ * issues caused by an uninitialized counter.
+ *
+ * Parameters:
+ * - None.
+ *
+ * Returns:
+ * - The 32-bit frequency value in Hz to be used by the platform timer.
+ */
 unsigned int plat_get_syscnt_freq2(void)
 {
 	uint32_t counter_freq = 0;
 	uint32_t ret = 0;
 
+	/* Read the counter frequency from the IOU_SCNTRS_BASE register */
 	counter_freq = mmio_read_32(IOU_SCNTRS_BASE +
 				    IOU_SCNTRS_BASE_FREQ_OFFSET);
+
+	/* If the counter frequency is non-zero, use it; otherwise, use cpu_clock */
 	if (counter_freq != 0U) {
 		ret = counter_freq;
 	} else {
+		/* Log a message indicating the counter frequency was not set */
 		INFO("Indicates counter frequency %dHz setting to %dHz\n",
 		     counter_freq, cpu_clock);
 		ret = cpu_clock;
