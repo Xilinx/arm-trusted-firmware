4c65515f1798a4feee4cc80a66af1625869fa79a
Maheedhar Bollapalli <maheedharsai.bollapalli@amd.com>
2026-03-20 12:48:08 +0530

    feat(amd): add support for ns-tl overlays
    Add support for exporting TF-A memory reservation as a DT overlay via
    the non-secure Transfer List. Instead of modifying the base DTB in place,
    TF-A generates a standard DT overlay that reserves its memory under
    /reserved-memory. This avoids DT size and alignment risks in secure world
    and leverages existing overlay support in BL33 (U-Boot).
    The overlay is skipped if the reservation is already present, preventing
    duplicate entries.
    
    The overlay compatible string is derived from the base DT root node at
    runtime to ensure compatibility checks are through in BL33 (U-Boot).
    
    An 8-byte subtype offset is prepended to the FDT overlay blob as part
    of overlay entry addition of the Transfer List to keep it consistent
    with the BLOBLIST feature.
    
    Change-Id: Ib674d3c4cab264413e4391302aab9b7d69b177f7
    Signed-off-by: Maheedhar Bollapalli <maheedharsai.bollapalli@amd.com>
    State: waiting
    Link: https://review.trustedfirmware.org/c/TF-A/trusted-firmware-a/+/50278

--- plat/amd/common/include/plat_xfer_list.h
+++ plat/amd/common/include/plat_xfer_list.h
@@ -9,6 +9,15 @@
 
 #include <transfer_list.h>
 
+/* TL overlay TAG is added if not defined already */
+#ifndef TL_TAG_FDT_OVRLY
+#define TL_TAG_FDT_OVRLY		7U
+#endif
+
+/* 8-byte subtype prepended to the DTBO blob in TL_TAG_FDT_OVRLY entry */
+#define TL_FDT_OVRLY_SUBTYPE_SIZE	sizeof(uint64_t)
+#define TL_FDT_OVRLY_SUBTYPE		0ULL
+
 int32_t transfer_list_populate_ep_info(entry_point_info_t *bl32,
 				       entry_point_info_t *bl33);
 
@@ -16,5 +25,6 @@ void *transfer_list_retrieve_dt_address(void);
 bool init_transfer_list_from_fdt_or_static(void);
 int32_t tl_init_ns_transfer_list(entry_point_info_t *bl33_image);
 int32_t tl_populate_ns_transfer_list(void);
+int32_t tl_add_dt_overlay(void *fdt, size_t fdtsize);
 
 #endif /* PLAT_XFER_LIST_H */
