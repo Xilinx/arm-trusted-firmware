66192aabfc1b98bdfab7658f59949c6c35d93c21
Suraj Kakade <suraj.hanumantkakade@amd.com>
2026-04-02 15:17:34 +0530

    fix(versal): fixing HIS_COMF violations
    This corrects HIS_COMF violation:
    Relationship of the number of comments (outside of and within functions)
    to the number of statements.
    
    Change-Id: I058207f8fe3d421f26cdfc3bfa63268150c3fbf4
    Signed-off-by: Suraj Kakade <suraj.hanumantkakade@amd.com>
    State: not-upstreamable (fusa)
    Link: https://review.trustedfirmware.org/c/TF-A/trusted-firmware-a/+/49767

--- plat/xilinx/versal/plat_topology.c
+++ plat/xilinx/versal/plat_topology.c
@@ -9,7 +9,20 @@
 
 static const uint8_t plat_power_domain_tree_desc[] = {1, PLATFORM_CORE_COUNT};
 
+/*
+ * plat_get_power_domain_tree_desc : Retrieves the platform's power domain tree descriptor.
+ * The power domain tree descriptor specifies the hierarchy of power domains.
+ * The first element represents the number of system level power domain.
+ * followed by the number of cores at the next level.
+ *
+ * Parameters:
+ *  - None.
+ *
+ * Return:
+ *  - const uint8_t *: Pointer to the power domain tree descriptor array.
+ */
 const uint8_t *plat_get_power_domain_tree_desc(void)
 {
+	/* Return the platform's power domain tree descriptor */
 	return plat_power_domain_tree_desc;
 }
