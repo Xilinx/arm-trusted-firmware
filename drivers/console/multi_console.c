ad31fbd703bed4a19ae67b52b1321c51a1d43fe4
Suraj Kakade <suraj.hanumantkakade@amd.com>
2026-04-02 15:08:27 +0530

    fix(console): fixing HIS_COMF violaitons
    This corrects HIS_COMF violation:
    Relationship of the number of comments (outside of and within functions)
    to the number of statements.
    
    Change-Id: I70ef780e5a3fa90ad71288ecfb13e19893316f1a
    Signed-off-by: Suraj Kakade <suraj.hanumantkakade@amd.com>
    State: not-upstreamable (fusa)
    Link: https://review.trustedfirmware.org/c/TF-A/trusted-firmware-a/+/49766

--- drivers/console/multi_console.c
+++ drivers/console/multi_console.c
@@ -49,26 +49,66 @@ console_t *console_unregister(console_t *to_be_deleted)
 	return NULL;
 }
 
+/*
+ * console_is_registered - Check if a console is already registered.
+ *
+ * This function traverses the global linked list of registered consoles
+ * (console_list) to determine if the specified console_t object is already
+ * present. It helps prevent duplicate registration of the same console
+ * instance, which could otherwise lead to list corruption or undefined
+ * behavior in the console framework.
+ *
+ * Parameters:
+ *   @to_find: Pointer to the console_t object to search for in the list.
+ *             Must not be NULL.
+ *
+ * Returns:
+ *   1 - If the console is found in the list (already registered).
+ *   0 - If the console is not found (not registered).
+ */
 int console_is_registered(console_t *to_find)
 {
 	console_t *console;
 
+	/* Check for valid input */
 	assert(to_find != NULL);
 
+	/* Traverse the linked list of registered consoles */
 	for (console = console_list; console != NULL; console = console->next) {
+		/* Check if the current console matches the one we're looking for */
 		if (console == to_find) {
-			return 1;
+			return 1; /* Match found, return 1 */
 		}
 	}
 
-	return 0;
+	return 0; /* No match found, return 0 */
 }
 
+/*
+ * console_switch_state - This function switches the current state of the console to
+ * the specified new state of the console framework.
+ *
+ * Parameters:
+ * @new_state: The new state to set for the console framework.
+ *
+ * Returns:
+ * None.
+ */
 void console_switch_state(unsigned int new_state)
 {
 	console_state = new_state;
 }
 
+/*
+ * console_set_scope - Sets the scope of a console.
+ *
+ * Parameters:
+ * @console: Pointer to the console structure whose scope is being updated.
+ * @scope: The new scope to set for the console.
+ *
+ * Returns:
+ * None.
+ */
 void console_set_scope(console_t *console, unsigned int scope)
 {
 	assert(console != NULL);
@@ -76,6 +116,17 @@ void console_set_scope(console_t *console, unsigned int scope)
 	console->flags = (console->flags & ~CONSOLE_FLAG_SCOPE_MASK) | scope;
 }
 
+/*
+ * do_putc - Writes a character to the specified console.
+ *
+ * Parameters:
+ * @c: The character to be written.
+ * @console: Pointer to the console structure where the character will be written.
+ *
+ * Returns:
+ * 0 or positive value - If the character is successfully written.
+ * Negative value      - If an error occurs during the write operation.
+ */
 static int do_putc(int c, console_t *console)
 {
 	int ret;
@@ -90,22 +141,53 @@ static int do_putc(int c, console_t *console)
 	return console->putc(c, console);
 }
 
+/*
+ * console_putc - Writes a character to all active consoles.
+ *
+ * This function iterates through the list of registered consoles and attempts to
+ * write the specified character to each console that is both active and has a
+ * valid `putc` function pointer. The function records the most appropriate return
+ * value based on the success or failure of the write attempts.
+ *
+ * Parameters:
+ * @c: The character to be written.
+ *
+ * Returns:
+ * 0              - If the character is successfully written to at least one console.
+ * Negative value - If no valid console is available or an error occurs during
+ *                  the write operation.
+ */
 int console_putc(int c)
 {
-	int err = ERROR_NO_VALID_CONSOLE;
+	int err = ERROR_NO_VALID_CONSOLE;  /* Default to failure state */
 	console_t *console;
 
+	/*
+	 * If the character is not printable, we don't write it to the console.
+	 * This is to avoid writing control characters that may not be handled
+	 * correctly by all consoles.
+	 */
 	for (console = console_list; console != NULL; console = console->next) {
 		if (((console->flags & console_state) != 0U) && (console->putc != NULL)) {
-			int ret = do_putc(c, console);
+			int ret = do_putc(c, console); /* Write to active console */
 			if ((err == ERROR_NO_VALID_CONSOLE) || (ret < err)) {
-				err = ret;
+				err = ret; /* Update error state */
 			}
 		}
 	}
-	return err;
+	return err; /* Return the error state, which is 0 if at least one console succeeded */
 }
 
+/*
+ * putchar - Writes a character to all active consoles.
+ *
+ * Parameters:
+ * @c: The character to be written.
+ *
+ * Returns:
+ * The character (`c`) - If the character is successfully written.
+ * EOF                 - If no valid console is available or an error occurs.
+ */
 int putchar(int c)
 {
 	int ret = 0;
@@ -143,13 +225,29 @@ int console_getc(void)
 }
 #endif
 
+/*
+ * console_flush - Flushes all active consoles.
+ *
+ * This function iterates through the list of registered consoles and invokes
+ * the flush operation for each console that is currently active and has a
+ * valid `flush` function pointer. Flushing ensures that any buffered output
+ * data is written out, maintaining output consistency.
+ *
+ * Parameters:
+ * None.
+ *
+ * Returns:
+ * None.
+ */
 void console_flush(void)
 {
 	console_t *console;
 
+	/* Travere the linked list of registered consoles */
 	for (console = console_list; console != NULL; console = console->next) {
+		/* Check if the console is active and supports flushing */
 		if (((console->flags & console_state) != 0U) && (console->flush != NULL)) {
-			console->flush(console);
+			console->flush(console); /* Call the flush function for the console */
 		}
 	}
 }
